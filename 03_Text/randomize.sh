888b6ddd4e3033d0e47a3937fadab66d10fb9cc3
Васильева Вера <veravasilieva14@mail.ru>
2025-10-07 03:25:16 +0300

    3: Add support russian and centering

--- 03_Text/randomize.sh
+++ 03_Text/randomize.sh
@@ -1,52 +1,74 @@
 #!/bin/sh
+
 exit_handler() {
   trap - EXIT
-  test -r "$TMPFILE" && rm -f "$TMPFILE"
-  test -r "$TMPFILE1" && rm -f "$TMPFILE1"
-  test -r "$TMPFILE2" && rm -f "$TMPFILE2"
+  test -r "$TMPFILE" && rm -vf "$TMPFILE" 
+  test -r "$TMPFILE1" && rm -vf "$TMPFILE1"
+  test -r "$TMPFILE2" && rm -vf "$TMPFILE2"
+  test -r "$TMPFILE3" && rm -vf "$TMPFILE3"
+  exit 1
 }
 
-NEWLINE=`echo -n "\n"| od -An -b -v -w1`
-SPACE=`echo -n " "| od -An -b -v -w1`
+NEWLINE=`echo -n "\n" | iconv -t UCS2 | od -An -b -v -w2`
+SPACE=`echo -n " " | iconv -t UCS2 | od -An -b -v -w2`
+NEWLINE=${NEWLINE%" "*}${NEWLINE##*" "} 
+SPACE=${SPACE%" "*}${SPACE##*" "} 
+
+TOTAL_SIZE=`stty size < /dev/tty`
+TOTAL_H=${TOTAL_SIZE%" "*}
+TOTAL_W=${TOTAL_SIZE##*" "} 
+
 
 trap exit_handler EXIT HUP INT QUIT PIPE TERM
 TMPFILE="`mktemp`"
 TMPFILE1="`mktemp`"
 TMPFILE2="`mktemp`"
+TMPFILE3="`mktemp`"
 
-X=0
-Y=0
+
 if [ -n "$1" ]; then
 	DELAY=$1
 else
 	DELAY=0.1
 fi
 
-od -An -b -v -w1 >> $TMPFILE
-while read SYMBOL
+tee $TMPFILE3 | iconv -t UCS2 | od -An -b -v -w2 > $TMPFILE
+
+SIZE=`cat $TMPFILE3 | wc -L -l`
+H=${SIZE%" "*}
+W=${SIZE##*" "} 
+
+INIT_X=$((TOTAL_W-W))
+INIT_X=$((INIT_X/2))
+X=$INIT_X
+Y=$((TOTAL_H-H))
+Y=$((Y/2))
+
+while read var1 var2 
 do
-    if [ "$SYMBOL" -ne "$NEWLINE" ]; then  
-        if [ "$SYMBOL" -ne "$SPACE" ]; then  
-            echo $SYMBOL $X $Y >> $TMPFILE1
+    if [ "$var1$var2" -ne "$NEWLINE" ]; then  
+        if [ "$var1$var2" -ne "$SPACE" ]; then  
+            echo $var1 $var2 $X $Y >> $TMPFILE1
         fi
         X=$((X+1))
     else
         Y=$((Y+1))
-        X=0
+        X=$INIT_X
     fi
 done < $TMPFILE
 
-cat $TMPFILE1
 shuf $TMPFILE1 -o $TMPFILE2
 clear
-while read var1 var2 var3 
+while read var1 var4 var2 var3 
 do 
 	tput cup $var3 $var2
-	echo -n "\\0$var1"
+	echo -n "\\$var1\\$var4" | iconv -f UCS2
 	sleep $DELAY
 
 done < $TMPFILE2
-tput cup $Y $X
-test -r "$TMPFILE" && rm -f "$TMPFILE"
-test -r "$TMPFILE1" && rm -f "$TMPFILE1"
-test -r "$TMPFILE2" && rm -f "$TMPFILE2"
+tput cup $Y 0
+
+test -r "$TMPFILE" && rm -vf "$TMPFILE"
+test -r "$TMPFILE1" && rm -vf "$TMPFILE1"
+test -r "$TMPFILE2" && rm -vf "$TMPFILE2"
+test -r "$TMPFILE3" && rm -vf "$TMPFILE3"
